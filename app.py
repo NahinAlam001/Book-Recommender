fce36334955090b0fef2655e1053f94818a33749
Nahin Alam <nahin.alam@northsouth.edu>
2024-01-23 15:26:01 +0600

    Update app.py

--- app.py
+++ app.py
@@ -77,20 +77,24 @@ selected_books = st.selectbox(
 
 if st.button('Show Recommendation'):
     recommended_books, poster_url = recommend_book(selected_books, model, book_pivot, final_rating)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_books[1])
-        st.image(poster_url[1])
-    with col2:
-        st.text(recommended_books[2])
-        st.image(poster_url[2])
 
-    with col3:
-        st.text(recommended_books[3])
-        st.image(poster_url[3])
-    with col4:
-        st.text(recommended_books[4])
-        st.image(poster_url[4])
-    with col5:
-        st.text(recommended_books[5])
-        st.image(poster_url[5])
+    if poster_url:
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            st.text(recommended_books[1])
+            st.image(poster_url[1])
+        with col2:
+            st.text(recommended_books[2])
+            st.image(poster_url[2])
+
+        with col3:
+            st.text(recommended_books[3])
+            st.image(poster_url[3])
+        with col4:
+            st.text(recommended_books[4])
+            st.image(poster_url[4])
+        with col5:
+            st.text(recommended_books[5])
+            st.image(poster_url[5])
+    else:
+        st.warning("No posters available for recommendations.")
