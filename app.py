62e80b1bec412cce7ead62d77c9dab140adf7e27
Nahin Alam <nahin.alam@northsouth.edu>
2024-01-23 16:06:03 +0600

    initial commit

--- app.py
+++ app.py
@@ -1,74 +1,49 @@
-import os
 import pickle
 import streamlit as st
 import numpy as np
 
-def load_data():
-    # Get the directory where this script is located
-    base_path = os.path.dirname(os.path.abspath(__file__))
 
-    model_path = os.path.join(base_path, 'artifacts', 'model.pkl')
-    book_names_path = os.path.join(base_path, 'artifacts', 'book_names.pkl')
-    final_rating_path = os.path.join(base_path, 'artifacts', 'final_rating.pkl')
-    book_pivot_path = os.path.join(base_path, 'artifacts', 'book_pivot.pkl')
+st.header('Book Recommender System Using Machine Learning')
+model = pickle.load(open('artifacts/model.pkl','rb'))
+book_names = pickle.load(open('artifacts/book_names.pkl','rb'))
+final_rating = pickle.load(open('artifacts/final_rating.pkl','rb'))
+book_pivot = pickle.load(open('artifacts/book_pivot.pkl','rb'))
 
-    try:
-        with open(model_path, 'rb') as model_file:
-            model = pickle.load(model_file)
 
-        book_names = pickle.load(open(book_names_path, 'rb'))
-        final_rating = pickle.load(open(final_rating_path, 'rb'))
-        book_pivot = pickle.load(open(book_pivot_path, 'rb'))
-    except FileNotFoundError as e:
-        st.error(f"Error loading file: {e}")
-        return None, None, None, None
-
-    return model, book_names, final_rating, book_pivot
-
-def fetch_poster(suggestion, final_rating):
+def fetch_poster(suggestion):
     book_name = []
     ids_index = []
     poster_url = []
 
     for book_id in suggestion:
-        book_name.append(final_rating.index[book_id])
+        book_name.append(book_pivot.index[book_id])
 
-    if not book_name:
-        st.warning("No book recommendations found.")
-        return []
-
-    for name in book_name[0]:
-        ids = np.where(final_rating['title'] == name)[0]
-        if ids.size > 0:
-            idx = ids[0]
-            if 0 <= idx < len(final_rating):
-                ids_index.append(idx)
+    for name in book_name[0]: 
+        ids = np.where(final_rating['title'] == name)[0][0]
+        ids_index.append(ids)
 
     for idx in ids_index:
-        url = final_rating.iloc[idx].get('img_url', 'default_image.png')
+        url = final_rating.iloc[idx]['image_url']
         poster_url.append(url)
 
     return poster_url
 
-def recommend_book(book_name, model, book_pivot, final_rating):
+
+
+def recommend_book(book_name):
     books_list = []
     book_id = np.where(book_pivot.index == book_name)[0][0]
-    distance, suggestion = model.kneighbors(book_pivot.iloc[book_id, :].values.reshape(1, -1), n_neighbors=6)
+    distance, suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors=6 )
 
-    poster_url = fetch_poster(suggestion, final_rating)
-
+    poster_url = fetch_poster(suggestion)
+    
     for i in range(len(suggestion)):
-        books = book_pivot.index[suggestion[i]]
-        for j in books:
-            books_list.append(j)
+            books = book_pivot.index[suggestion[i]]
+            for j in books:
+                books_list.append(j)
+    return books_list , poster_url       
 
-    return books_list, poster_url
 
-# Streamlit app
-st.header('Book Recommender System Using Machine Learning')
-
-# Load data
-model, book_names, final_rating, book_pivot = load_data()
 
 selected_books = st.selectbox(
     "Type or select a book from the dropdown",
@@ -76,25 +51,21 @@ selected_books = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_books, poster_url = recommend_book(selected_books, model, book_pivot, final_rating)
+    recommended_books,poster_url = recommend_book(selected_books)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_books[1])
+        st.image(poster_url[1])
+    with col2:
+        st.text(recommended_books[2])
+        st.image(poster_url[2])
 
-    if poster_url:
-        col1, col2, col3, col4, col5 = st.columns(5)
-        with col1:
-            st.text(recommended_books[1])
-            st.image(poster_url[1])
-        with col2:
-            st.text(recommended_books[2])
-            st.image(poster_url[2])
-
-        with col3:
-            st.text(recommended_books[3])
-            st.image(poster_url[3])
-        with col4:
-            st.text(recommended_books[4])
-            st.image(poster_url[4])
-        with col5:
-            st.text(recommended_books[5])
-            st.image(poster_url[5])
-    else:
-        st.warning("No posters available for recommendations.")
+    with col3:
+        st.text(recommended_books[3])
+        st.image(poster_url[3])
+    with col4:
+        st.text(recommended_books[4])
+        st.image(poster_url[4])
+    with col5:
+        st.text(recommended_books[5])
+        st.image(poster_url[5])
